d9c5ddd6b02d765222784838a84710217f6e4a05
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-21 18:11:05 -0800

    Outline steps for game

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rock_paper_scissors
+
+Create Rock Paper Scissors Game
+Game will be played in console on a blank html doc
---
+++ game.js
@@ -0,0 +1,10 @@
+console.log("hello World")
+
+// Create function to get computer choice for move
+// Create function to get player selection for choice
+// Create function for computer selection
+// Return string of if you win/lose and player and computer chocie of move
+
+function getComputerChoice() {
+    pass
+}
