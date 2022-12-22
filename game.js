58af1f3f2ea74318e01255c6985b76f5df80df47
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-21 18:53:22 -0800

    function getComputerChoice

--- game.js
+++ game.js
@@ -6,5 +6,16 @@ console.log("hello World")
 // Return string of if you win/lose and player and computer chocie of move
 
 function getComputerChoice() {
-    pass
+    let number = Math.random();
+    console.log(number)
+    if (number < 0.33) {
+        var computerChoice = "Rock";
+    }   else if (number > 0.33 && number < 0.66) {
+        var computerChoice = "Paper";
+    } else {
+        var computerChoice = "Scissors";
+    }
+    return computerChoice;
 }
+
+console.log(getComputerChoice());
