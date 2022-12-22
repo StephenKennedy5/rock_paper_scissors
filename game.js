b51156885c15f42c57c011a5989b4d7d88ff32dd
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-21 19:36:47 -0800

    Added game() function

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-console.log("hello World")
+console.log("To play game type 'game()' in the console");
 
 // Create function to get computer choice for move
 // Create function to get player selection for choice
@@ -7,7 +7,6 @@ console.log("hello World")
 
 function getComputerChoice() {
     let number = Math.random();
-    console.log(number)
     if (number < 0.33) {
         var computerChoice = "Rock";
     }   else if (number > 0.33 && number < 0.66) {
@@ -18,4 +17,51 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection,computerSelection);
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Rock") {
+            console.log(`It's a tie. ${playerSelection} ties ${computerSelection}.`);
+        } else if (computerSelection === "Scissors") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}.`);
+        } else {
+            console.log(`You Lose. ${computerSelection} beats ${playerSelection}.`);
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Scissors") {
+            console.log(`It's a tie. ${playerSelection} ties ${computerSelection}.`);
+        } else if (computerSelection === "Paper") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}.`);
+        } else {
+            console.log(`You Lose. ${computerSelection} beats ${playerSelection}.`);
+        }
+    } else {
+        if (computerSelection === "Paper") {
+            console.log(`It's a tie. ${playerSelection} ties ${computerSelection}.`);
+        } else if (computerSelection === "Rock") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}.`);
+        } else {
+            console.log(`You Lose. ${computerSelection} beats ${playerSelection}.`);
+        }
+    }
+
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let userprompt = prompt("Want to play a game? Choose Rock, Paper, or Scissors. \nTo stop playing type Quit or Exit.");
+        let playerSelection = userprompt.charAt(0).toUpperCase() + userprompt.slice(1).toLocaleLowerCase();
+        let computerSelection = getComputerChoice();
+
+        if (playerSelection === "Rock" || playerSelection === "Paper" || playerSelection === "Scissors") {
+            console.log(playRound(playerSelection,computerSelection));
+        } else if (playerSelection === "Quit" || playerSelection === "Exit") {
+            break;
+        } else {
+
+        }
+
+    }
+}
+
+game();
