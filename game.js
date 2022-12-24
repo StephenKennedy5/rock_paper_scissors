28f31f396a8a4ac22ac417ca609fa0eeb6b96da8
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-23 17:30:45 -0800

    Update html with score and alert to winner

--- game.js
+++ game.js
@@ -1,12 +1,3 @@
-console.log("To play game type 'game()' in the console");
-
-const buttons = document.querySelectorAll('.choice');
-
-buttons.forEach(button => button.addEventListener('click', function() {
-    console.log(button.innerHTML);
-    console.log(playRound(button.innerHTML));
-}))
-
 // Create function to get computer choice for move
 // Create function to get player selection for choice
 // Create function for computer selection
@@ -16,6 +7,19 @@ buttons.forEach(button => button.addEventListener('click', function() {
 // rewrite playRound so call getComputerChoice
 // eventlistener calls playRound
 // be able to play up til 5 rounds and keep track of score
+
+console.log("To play game type 'game()' in the console");
+
+const buttons = document.querySelectorAll('.choice');
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+buttons.forEach(button => button.addEventListener('click', function() {
+    console.log(button.innerHTML);
+    console.log(game(button.innerHTML));
+}))
+
 
 function getComputerChoice() {
     let number = Math.random();
@@ -34,52 +38,14 @@ function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
     console.log(playerSelection,computerSelection)
     if (playerSelection === computerSelection) {
-        return "Tie"
+        return "Tie";
     } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
                 (playerSelection === "Paper" && computerSelection === "Rock") ||
                 (playerSelection === "Scissors" && computerSelection === "Paper")) {
-                    return "Player"
+                    return "Player";
     }   else {
-        return "Computer"
+        return "Computer";
     }
-
-
-    // if (playerSelection === "Rock") {
-    //     if (computerSelection === "Rock") {
-    //         console.log(`It's a tie. ${playerSelection} ties ${computerSelection}.`);
-    //         return "Tie";
-    //     } else if (computerSelection === "Scissors") {
-    //         console.log(`You Win! ${playerSelection} beats ${computerSelection}.`);
-    //         return "Player"
-    //     } else {
-    //         console.log(`You Lose. ${computerSelection} beats ${playerSelection}.`);
-    //         return "Computer"
-    //     }
-    // } else if (playerSelection === "Scissors") {
-    //     if (computerSelection === "Scissors") {
-    //         console.log(`It's a tie. ${playerSelection} ties ${computerSelection}.`);
-    //         return "Tie"
-    //     } else if (computerSelection === "Paper") {
-    //         console.log(`You Win! ${playerSelection} beats ${computerSelection}.`);
-    //         return "Player"
-    //     } else {
-    //         console.log(`You Lose. ${computerSelection} beats ${playerSelection}.`);
-    //         return "Computer"
-    //     }
-    // } else if (playerSelection === "Paper"){
-    //     if (computerSelection === "Paper") {
-    //         console.log(`It's a tie. ${playerSelection} ties ${computerSelection}.`);
-    //         return "Tie"
-    //     } else if (computerSelection === "Rock") {
-    //         console.log(`You Win! ${playerSelection} beats ${computerSelection}.`);
-    //         return "Player"
-    //     } else {
-    //         console.log(`You Lose. ${computerSelection} beats ${playerSelection}.`);
-    //         return "Computer"
-    //     }
-    // } else {
-    //     console.log("Player did not make a move.")
-    // }
 
 }
 
@@ -112,21 +78,14 @@ function playRound(playerSelection) {
 //     console.log(`Final Score: Player - ${playerScore}, Computer - ${computerScore}, Ties - ${tieScore}`)
 // }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let tieScore = 0;
-    let userprompt = prompt(`Want to play a game? Choose Rock, Paper, or Scissors.\nThe Score is ${playerScore}-${computerScore}-${tieScore} \nTo stop playing type Quit or Exit.`);
-    let playerSelection = userprompt.charAt(0).toUpperCase() + userprompt.slice(1).toLocaleLowerCase();
-    let computerSelection = getComputerChoice();
+function game(playerSelection) {
+    var outcome = playRound(playerSelection);
+    
+    update_score(outcome);
+}
 
-    if (playerSelection === "Rock" || playerSelection === "Paper" || playerSelection === "Scissors") {
-        var outcome = playRound(playerSelection,computerSelection);
-    } else {
-        let userprompt = prompt("You did not choose a proper choice for the game. \nPlease choose Rock, Paper, or Scissors.");
-        let playerSelection = userprompt.charAt(0).toUpperCase() + userprompt.slice(1).toLocaleLowerCase();
-        console.log(playRound(playerSelection,computerSelection));
-    }
+function update_score(outcome) {
+    
     if (outcome === "Tie") {
         tieScore++;
     } else if (outcome === "Player") {
@@ -134,7 +93,21 @@ function game() {
     } else {
         computerScore++;
     }
-    console.log(`Final Score: Player - ${playerScore}, Computer - ${computerScore}, Ties - ${tieScore}`)
+
+    document.getElementById("score").innerHTML = `Your Score ${playerScore} - CPU Score ${computerScore} - ${tieScore}`;
+        
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5) {
+            window.alert("Winner is YOU!");
+        } else {
+            window.alert("Winner is CPU");
+        }
+        tieScore = 0;
+        playerScore = 0;
+        computerScore = 0;
+        document.getElementById("score").innerHTML = `${playerScore} - ${computerScore} - ${tieScore}`;
+    }
+
 }
 
 
